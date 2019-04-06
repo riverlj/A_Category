1baddec10f4a6987568e821c432f843bc9d6099d
lijiang <lijiang@xiaoshouyi.com>
2019-04-06 17:19:25 +0800

    Initial Commit

---
+++ A_Category/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  A_Category
+//
+//  Created by 李江 on 2019/4/6.
+//  Copyright © 2019 李江. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ A_CategoryTests/A_CategoryTests.m
@@ -0,0 +1,37 @@
+//
+//  A_CategoryTests.m
+//  A_CategoryTests
+//
+//  Created by 李江 on 2019/4/6.
+//  Copyright © 2019 李江. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface A_CategoryTests : XCTestCase
+
+@end
+
+@implementation A_CategoryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
